98fbf1dbbd227a838bc51bcb7cd921646bcdb91b
Faisal Ahmed Sifat <Faisalahmed531@gmail.com>
2022-04-29 21:42:44 +0600

    added Shared Preferences to store the data locally

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:code_companion/views/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async{
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:add_2_calendar/add_2_calendar.dart';
 import 'package:code_companion/models/contests.dart';
 import 'package:code_companion/services/remote_services.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,21 +18,54 @@ class _HomePageState extends State<HomePage> {
   Contests? contests;
   var isLoaded = false;
   List<Result>? result;
-  List<Result> ans = [];
+  List<Result> ans = [
+  ];
+  // final box = GetStorage();
+  String lastUpdated = "";
 
   @override
   void initState() {
     super.initState();
     // compareTime(1650378900);
+    // readStorage();
+    setState(() {
+      retrieveData();
+      isLoaded = true;
+    });
     getContests();
   }
 
+  retrieveData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? stringData = prefs.getString('data');
+    List dataList = jsonDecode(stringData!);
+
+    for (var data in dataList) {
+      print(Result.fromJson(data));
+      setState(() {
+        ans.add(Result.fromJson(data));
+        isLoaded = true;
+      });
+    }
+  }
+
+  storeData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List dataList = ans.map((e) => e.toJson()).toList();
+    // print(dataList);
+    prefs.setString('data', jsonEncode(dataList));
+  }
+
   getContests() async {
+    // final SharedPreferences prefs = await SharedPreferences.getInstance();
+    // await jsonDecode(prefs.getString('data') as String);
+    // print(await jsonDecode(prefs.getString('data') as String));
+
     contests = await RemoteService().getContests();
     result = contests?.result;
     if (contests != null) {
       var today = DateTime.now();
-
+      ans = [];
       for (var res in result!) {
         if (today.isBefore(
             DateTime.fromMillisecondsSinceEpoch(res.startTimeSeconds * 1000))) {
@@ -37,11 +73,28 @@ class _HomePageState extends State<HomePage> {
         }
       }
       ans.sort((a, b) => a.startTimeSeconds.compareTo(b.startTimeSeconds));
+      storeData();
       setState(() {
         isLoaded = true;
+        lastUpdated = DateTime.now().toString();
+        // writeStorage();
       });
     }
   }
+
+  // writeStorage() {
+  //   box.write("storage", ans);
+  //   print(ans);
+  // }
+
+  // readStorage() {
+  //   ans = box.read("storage");
+  //   if (ans != null) {
+  //     setState(() {
+  //       isLoaded = true;
+  //     });
+  //   }
+  // }
 
   getTime(int time) {
     DateTime date = DateTime.fromMillisecondsSinceEpoch(time * 1000);
